0116efe374a2a6b43baab41a66ed528aaad5ffff
Vlad Yaglamunov <VladYaglamunov@gmail.com>
2017-11-27 14:35:49 +0300

    PogChamp

---
+++ Assets/Complex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+class Complex {
+    public double x, y;
+
+    public Complex(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public static Complex operator *(Complex a, double k) {
+        return new Complex(a.x * k, a.y * k);
+    }
+
+    public static Complex operator *(Complex a, Complex b) {
+        return new Complex(a.x * b.x - a.y * b.y, a.y * b.x + a.x * b.y);
+    }
+
+    public static Complex operator +(Complex a, Complex b) {
+        return new Complex(a.x * b.x, a.y * b.y);
+    }
+
+    // Square distanse between two points
+    public static double operator %(Complex a, Complex b) {
+        return Math.Pow(a.x - b.x, 2) + Math.Pow(a.y - b.y, 2);
+    }
+
+    public Complex z2() {
+        return new Complex(Math.Pow(x, 2) - Math.Pow(y, 2), 2.0 * x * y);
+    }
+
+    public Complex z3() {
+        return new Complex(Math.Pow(x, 3) - 3.0 * x * Math.Pow(y, 2), 3.0 * Math.Pow(x, 2) * y - Math.Pow(y, 3));
+    }
+
+    public static explicit operator Vector3(Complex a) {
+        return new Vector3((float) a.x, (float) a.y, 1);
+    }
+}
---
+++ Assets/Newton.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Newton : MonoBehaviour {
+    private LineRenderer line;
+
+    public int Aproximation = 10;
+    public double Scale = 1.5;
+    public int Parts = 10;
+
+    // Use this for initialization
+    void Start() {
+        line = GetComponent<LineRenderer>();
+
+//        line.positionCount = 5;
+//        line.SetPositions(new Vector3[5] {new Vector3(0, 0, 1), new Vector3(0, 1, 1), new Vector3(1, 1, 1), new Vector3(2, -1, 1), new Vector3(3, -1, 1)});
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var tmp = new Complex(0.0, 0.0);
+        var b = new Complex(-Scale, -Scale);
+        for (var i = 0; i < Parts; i++) {
+            tmp.x = b.x + 2.0 * Scale * i / Parts;
+            for (var j = 0; j < Parts; j++) {
+                tmp.y = b.y + 2.0 * Scale * j / Parts;
+                line.SetPositions(Calc(tmp).Select(x => (Vector3) x).ToArray());
+            }
+        }
+    }
+
+    private List<Complex> Calc(Complex number) {
+        var nearness = new List<Complex> {number};
+
+        for (var i = 1; i < Aproximation; i++) {
+            var prev = nearness[i - 1];
+
+            var z3 = prev.z3();
+            var z2 = prev.z2();
+
+            z3 *= 2.0;
+            z3.x += 1;
+
+            var nz2 = new Complex(z2.x, -z2.y);
+            z3 *= nz2;
+            z2 *= 3.0;
+            z2 *= nz2;
+
+            nearness.Add(z3 * (1 / z2.x));
+        }
+
+        return nearness;
+    }
+}
